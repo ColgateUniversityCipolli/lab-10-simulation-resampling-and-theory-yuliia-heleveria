6649f95a1ba44fa893f1c36e221907e979b11919
yuliia-heleveria <148816812+yuliia-heleveria@users.noreply.github.com>
2025-04-01 14:53:35 -0400

    Completed task 1

---
+++ Lab10.R
@@ -0,0 +1,62 @@
+################################################################################
+# LAB 10 R CODE
+# YULIIA HELEVERIA
+# MATH 240 - SPRING 2025
+################################################################################
+
+################################################################################
+# Load Libraries
+################################################################################
+library(tidyverse)
+library(ggplot2)
+
+################################################################################
+# TASK 1: Basic Simulation
+################################################################################
+#generate 10k polls for satisfaction with US world position
+n.serveyed <- 1004 #sample size
+probability <- 0.39
+n.repeat <- 10000
+
+#generate the poll completion 10,000 times
+poll <- rbinom(n.repeat, n.serveyed, probability)
+
+#put data into tibble for plotting
+ggpoll <- tibble(poll)|>
+  mutate(poll = poll/n.serveyed)#convert the data into proportion
+
+#plot a histogram of sample proportion with superimposed density
+plot.poll <- ggplot(data = ggpoll)+
+  geom_histogram(aes(x = poll, y = after_stat(density)),
+                 color="grey",
+                 bins = 25)+ #histogram of generated polls
+  geom_density(aes(x = poll),
+               color = "red")+ #superimposed density
+  scale_x_continuous(breaks = seq(0.3, 0.5, by = 0.03))+ #put ticks on y-axis
+  geom_hline(yintercept = 0)+ #add y-intercept
+  theme_bw()+
+  xlab("Proportion of satisfied adult sample (n = 1004)")+ #add x and y label
+  ylab("Density")
+
+#double the sample size and perform the same computation
+double.serveyed <- 2008 #new sample size
+
+#generate the poll completion 10,000 times
+poll.double <- rbinom(n.repeat, double.serveyed, probability)
+
+#put data into tibble for plotting
+ggpoll.double <- tibble(poll.double)|>
+  mutate(poll = poll.double/double.serveyed) #convert the data into proportion
+
+#plot a histogram of sample proportion with superimposed density
+plot.poll.double <- ggplot(data = ggpoll.double)+
+  geom_histogram(aes(x = poll, y = after_stat(density)),
+                 color="grey",
+                 bins = 25)+ #histogram of generated polls
+  geom_density(aes(x = poll),
+               color = "red")+ #superimposed density
+  scale_x_continuous(breaks = seq(0.3, 0.5, by = 0.03))+ #put ticks on y-axis
+  geom_hline(yintercept = 0)+ #add y-intercept
+  theme_bw()+
+  xlab("Proportion of satisfied adult sample (n = 2008)")+ #add x and y label
+  ylab("Density")
